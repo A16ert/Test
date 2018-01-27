17dd7c6a27447b8f4821d981ba685572706961db
Albert <fajb420@gmail.com>
2018-01-28 00:01:17 +0300

    new ViewModel

---
+++ WpfApp1/WpfApp1/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private double _value;
+
+        public double Value {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+            
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string Name)
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs(Name) );
+        }
+        
+
+    }
+}
